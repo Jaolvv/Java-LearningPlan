66a90af2300f802e7672362dcdd70d29998b43c3
Liu <51476712+Jaolvv@users.noreply.github.com>
2021-03-22 15:54:02 +0800

    Delete 近期/练习1 directory

--- 近期/练习1/GreenBuble.java
+++
@@ -1,9 +0,0 @@
-package 面向对象.练习1;
-
-public class GreenBuble extends Buble {
-    //重写父类抽象方法
-
-    public String bright(){
-        return "绿灯发亮";
-    }
-}
--- 近期/练习1/RedBuble.java
+++
@@ -1,9 +0,0 @@
-package 面向对象.练习1;
-
-public class RedBuble extends Buble {
-    //重写父类ch抽象方法
-
-    public String bright(){
-        return "红灯发亮";
-    }
-}
